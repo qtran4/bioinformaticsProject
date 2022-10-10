28b17588a453c52f06d86822a99b34bd0f3de612
Peter Martin <pmarti23@crcfe02.crc.nd.edu>
2022-10-10 13:17:34 -0400

    Development

--- Proteome_analyzer.sh
+++ Proteome_analyzer.sh
@@ -1,9 +1,8 @@
 # 
 # Usage: bash Proteome_analyzer.sh mcrA_reference_files hsp70_reference_files proteome_sequences
 
-~/Private/Biocomputing2022/tools/muscle -in "$1" -out mcrA_alignment | ~/Private/Biocomputing2022/tools/hmmbuild mcrA_HMM
-
-for proteome in "$3"
-do
+# cat "$1" | ~/Private/Biocomputing2022/tools/muscle -out mcrA_alignment | ~/Private/Biocomputing2022/tools/hmmbuild mcrA_HMM
+cat ref_sequences/mcrAgene_*.fasta | ~/Private/Biocomputing2022/tools/muscle -out mcrA_alignment
+~/Private/Biocomputing2022/tools/hmmbuild mcrA_HMM mcrA_alignment | ~/Private/Biocomputing2022/tools/h
 
 
