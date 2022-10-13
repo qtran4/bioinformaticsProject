e322c28b5f88e472a0b275cd55dc5b98804cbb11
Peter Martin <pmarti23@crcfe02.crc.nd.edu>
2022-10-13 10:51:41 -0400

    Table creation code

--- Proteome_analyzer.sh
+++ Proteome_analyzer.sh
@@ -11,8 +11,6 @@ cat $1/mcrAgene_*.fasta | ~/Private/Biocomputing2022/tools/muscle -out mcrA_alig
 cat $1/hsp70gene_*.fasta | ~/Private/Biocomputing2022/tools/muscle -out hsp70_align.fasta
 ~/Private/Biocomputing2022/tools/hmmbuild hsp70_HMM hsp70_align.fasta
 
-#file=$(ls "$2" | wc -l)
-
 for proteome in $2/proteome_*.fasta
 do
 number=$(ls $proteome | grep -o "[0-9][0-9]")
@@ -20,3 +18,21 @@ echo $number
 ~/Private/Biocomputing2022/tools/hmmsearch --tblout mcrA_seqmatch$number mcrA_HMM $proteome
 ~/Private/Biocomputing2022/tools/hmmsearch --tblout hsp70_seqmatch$number hsp70_HMM $proteome
 done
+
+cat mcrA_seqmatch* > all_mcrA_matches
+cat hsp70_seqmatch* > all_hsp70_matches
+
+clear
+
+for mcrA_entry in mcrA_seqmatch*
+do
+grep -w -E "\s\-\s|Target file\: " $mcrA_entry | grep -o "proteome_[0-9][0-9]" >> proteome.txt
+grep -w -E "\s\-\s" $mcrA_entry | wc -l >> mcrA.txt 
+done
+
+for hsp70_entry in hsp70_seqmatch*
+do
+grep -w -E "\s\-\s" $hsp70_entry | wc -l >> hsp70.txt 
+done
+
+paste proteome.txt mcrA.txt hsp70.txt -d "," 
