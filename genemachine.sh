a426f6d64630f6db604f00b5e06271234dda9cbd
Therese Reisch <treisch@crcfe02.crc.nd.edu>
2022-10-12 20:51:19 -0400

    yoo we did it

--- genemachine.sh
+++ genemachine.sh
@@ -1,4 +1,4 @@
-#start in the bioinformaticsProject
+#start in the bioinformaticsProject, all of this code is assuming that out TA has the same directory structure as we do
 #this code can be used to put all of the mcrA reference files into one file and all of the hsp gene reference files into another file
 touch  ref_sequences/mcrAmaster.fasta
 for index in {01..18}
@@ -14,6 +14,27 @@ done
 ~/Private/biocomputing2022/tools/muscle -in ref_sequences/mcrAmaster.fasta -out ref_sequences/mcrAmaster.aligned
 ~/Private/biocomputing2022/tools/muscle -in ref_sequences/hsp70master.fasta -out ref_sequences/hsp70master.aligned
 
+#build things with hmmer
+~/Private/biocomputing2022/tools/hmmbuild ref_sequences/mcrAmaster.build ref_sequences/mcrAmaster.aligned
+~/Private/biocomputing2022/tools/hmmbuild ref_sequences/hsp70master.build ref_sequences/hsp70master.aligned
 
+#search mcrA with hmmr
+for index in {01..50}
+	do ~/Private/biocomputing2022/tools/hmmsearch --tblout proteomes/proteome$index.mcrAsearch ref_sequences/mcrAmaster.build proteomes/proteome_$index.fasta
+done
+ 
+#search hsp70 with hmmr
+for index in {01..50}
+        do ~/Private/biocomputing2022/tools/hmmsearch --tblout proteomes/proteome$index.hsp70search ref_sequences/hsp70master.build proteomes/proteome_$index.fasta
+done 
 
+#making the table textfile
+touch proteomecounttable.txt
+echo proteome_name, mcrA_count, hsp70_count >> proteomecounttable.txt
 
+#coounting the matches
+for index in {01..50}
+	do mcrAcount=$(grep -v -c "#" proteomes/proteome$index.mcrAsearch)
+	hsp70count=$(grep -v -c "#" proteomes/proteome$index.hsp70search)
+	echo "proteome_$index, $mcrAcount, $hsp70count" >> proteomecounttable.txt
+done
