155b9f14aa9b96ffc22a032bde2f4b28251aef9d
haydengallo <hgallo@nd.edu>
2022-10-14 11:48:18 -0400

    Very Close In Table Format Just Need to Email TA

--- bioinformatics_project_script.sh
+++ bioinformatics_project_script.sh
@@ -1,4 +1,4 @@
-#This script concatenates all of the possible gene combos then uses muslce to
+#This script concatenates all of the possible gene combos then uses muscle to
 # align them.
 
 # usage bash bioinformatics_project_script.sh
@@ -10,25 +10,21 @@ cat ./ref_sequences/mcr* > all_mcr.fasta
 ~/Private/Biocomputing2022/tools/hmmbuild hsp_hmmbuild_results hsp_muscle_output
 ~/Private/Biocomputing2022/tools/hmmbuild mcr_hmmbuild_results mcr_muscle_output
 
-echo "Proteome Recommendation List" >> recommendation_list
+echo "Proteome Number,Number of MCR Genes,Number of HSP Genes" > countfile_proteomes.csv; \
+echo "Proteome Number,Number of MCR Genes,Number of HSP Genes" > recommendation_list.csv; \
 
 for file in ./proteomes/*.fasta
 do
-echo $file | cut -c 13- | cut -c -11 >> countfile_proteomes 
+name=$(echo $file | cut -c 13- | cut -c -11) 
 ~/Private/Biocomputing2022/tools/hmmsearch --tblout temp_output_hsp hsp_hmmbuild_results $file
-echo "number of hsp genes:" >> countfile_proteomes
 hsp_count=$(grep -v -c '#' temp_output_hsp)
-echo $hsp_count >> countfile_proteomes
-#cat temp_output_hsp | hsp_count="$(grep -v -c '#')" | hsp_count  >> countfile_proteomes
 ~/Private/Biocomputing2022/tools/hmmsearch --tblout temp_output_mcr mcr_hmmbuild_results $file
-echo "number of mcr genes:" >> countfile_proteomes
 mcr_count=$(grep -v -c '#' temp_output_mcr) 
-echo $mcr_count >> countfile_proteomes
+echo "$name,$mcr_count,$hsp_count" >> countfile_proteomes.csv 
 
-if (( $hsp_count>=1 )) && (( $mcr_count>=1))
-then 
-     	echo $file | cut -c 13- | cut -c -11 >> recommendation_list
-fi
-#echo "{$file}_hmmbuild_results"
 done
 
+cat countfile_proteomes.csv | sort -t"," -k2rn,2 -k3rn,3 | grep -v ',0' >> recommendation_list.csv
+
+rm temp_output_hsp
+rm temp_output_mcr
